81eb12b763c4dfbb24ea40796afbfe0acd5f9a8f
Miriyan Asenov <mirorim.789@gmail.com>
2025-02-02 23:43:45 +0200

    New component - Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import { useState } from 'react';
 
-function Info({number}) {
-	return <h3>{number}</h3>;
+function Info({message}) {
+	return <h3>{message}</h3>;
 }
 
 function Button({handleOnClick, name}){
 	return <button onClick={handleOnClick}>{name}</button>;
 }
 
+function Input({inpVal, handleOnChange}){
+	return <input type="number" value={inpVal} onChange={handleOnChange} />; 
+}
+
 function App() {
 
   const [secretNumber, setSecretNumber] = useState(Math.trunc(Math.random() * 10) );
+  const [guess, setGuess] = useState('');
+
+  function handleChange(e){
+		setGuess(e.target.value); 
+	}
 
   function handleNewGame() {
-		setSecretNumber(Math.trunc(Math.random() * 10) ); 
+		setSecretNumber(Math.trunc(Math.random() * 10) );
+		setGuess('');  
 	}
-   
+
   return  (
     <>
-      <Info number={secretNumber} />
+      <Info message={guess === '' ? 'Guess the number' : secretNumber === Number(guess) ? 'Yes-' + secretNumber : 'No-' + secretNumber} />
+      <Input inpVal={guess} handleOnChange={handleChange}/>
       <Button handleOnClick={handleNewGame} name='New Game'/>
     </>  
   );
