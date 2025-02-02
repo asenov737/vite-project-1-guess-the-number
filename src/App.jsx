0aec8f331d71460ed192eeb23362cf900e76d0d5
Miriyan Asenov <mirorim.789@gmail.com>
2025-02-02 23:34:42 +0200

    Fundamental changes in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
-function Info({message}) {
-	return <h3>{message}</h3>;
+function Info({number}) {
+	return <h3>{number}</h3>;
 }
 
 function Button({handleOnClick, name}){
@@ -10,18 +10,21 @@ function Button({handleOnClick, name}){
 
 function App() {
 
-  const [secretNumber, setSecretNumber] = useState(Math.trunc(Math.random() * 100) + 1);
+  const [secretNumber, setSecretNumber] = useState(Math.trunc(Math.random() * 10) );
 
   function handleNewGame() {
-		setSecretNumber(Math.trunc(Math.random() * 100) + 1); 
+		setSecretNumber(Math.trunc(Math.random() * 10) ); 
 	}
    
   return  (
     <>
-      <Info message={secretNumber + ' is the secret number'} />
+      <Info number={secretNumber} />
       <Button handleOnClick={handleNewGame} name='New Game'/>
     </>  
   );
 }
 
 export default App;
+
+
+
